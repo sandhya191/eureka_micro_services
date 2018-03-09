1d3e9a59385e06886e8bb105db0a445299ff06bf
Sandhya <sandhyaj224@gmail.com>
2018-03-08 22:50:20 -0500

    Refractor

--- src/main/java/com/examples/web/NetflixEurekaNamingServerApplication.java
+++ src/main/java/com/examples/web/NetflixEurekaNamingServerApplication.java
@@ -4,6 +4,13 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
 
+/**
+ * 
+ * @author Sandhya
+ * Netflix Eureka Server Application of Project.
+ *
+ */
+
 @SpringBootApplication
 @EnableEurekaServer
 public class NetflixEurekaNamingServerApplication {
